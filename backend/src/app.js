6fd23685f2c6426147a9fc602ba768076c353140
internminah <internminah@gmail.com>
2026-06-22 14:18:12 +0530

    Initial project structure

---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    success: true,
+    message: 'Construction Company API Running'
+  });
+});
+
+module.exports = app;
